0826aa4afdc27a7584f1a45e2bd040bee63213f4
Lev Rekhlov <l.rekhlov@innopolis.university>
2022-02-28 13:57:37 +0300

    Update CurrencyConverter.java

--- src/main/java/ru/inno/ssdlab06/CurrencyConverter.java
+++ src/main/java/ru/inno/ssdlab06/CurrencyConverter.java
@@ -1,6 +1,24 @@
 package ru.inno.ssdlab06;
 
-public class CurrencyConverter {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
-  //TODO your code here
+public class CurrencyConverter {
+    public Map<String, Float> currencies = new HashMap<String, Float>() {{
+        put("usd", (float)83.5485);
+        put("eur", (float)93.5994);
+        put("aud", (float)60.0964);
+    }};
+
+    public float convert(float value, String from, String to) {
+        if (Objects.equals(from, "rouble")) {
+            return value / currencies.get("to");
+        } else if (Objects.equals(to, "rouble")) {
+            return value * currencies.get("from");
+        } else {
+            return this.convert(this.convert(value, from, "rouble"), "rouble", to);
+        }
+    }
 }
+
